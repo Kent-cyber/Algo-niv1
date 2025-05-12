cd45c1f7bc0e3c141f7f0b6acdcb941c4edd9426
Kent-cyber <mrkentfire96@gmail.com>
2025-05-12 12:09:43 +0200

    ajout d'une méthode pour convertir les monnaies

--- Exercices/exo15.java
+++ Exercices/exo15.java
@@ -36,44 +36,66 @@ import java.util.Scanner;
 
 public class exo15 {
     
-    public static void Euro_to_other(double amt) {
+    public static void Euro_to_other(double amt) { // Création d'une méthode pour convertir l'euro en d'autres monnaie
 
-        System.out.println(Math.round(amt)+" Euro = " + Math.round((amt*1.13*100.0)) / 100.0 + " Dollar");
+        System.out.println(Math.round(amt)+" Euro = " + Math.round(amt*1.13*100.0) / 100.0 + " Dollar");
         System.out.println();
 
-        System.out.println(Math.round(amt)+" Euro = " + Math.round((amt*0.85*100.0)) / 100.0 + " Livre sterling");
-    }
-
-    public static void Dollar_to_other(double amt) {
-
-        System.out.println(Math.round(amt)+" Dollar = " + Math.round((amt*0.88*100.0)) / 100.0 + " Euro");
+        System.out.println(Math.round(amt)+" Euro = " + Math.round(amt*0.85*100.0) / 100.0 + " Livre sterling");
         System.out.println();
 
-        System.out.println(Math.round(amt)+" Dollar = " + Math.round((amt*0.75*100.0)) / 100.0 + " Livre sterling");
+        System.out.println(Math.round(amt)+" Euro = " + Math.round(amt*163.07*100.0) / 100.0 + " Yen");
     }
 
-    public static void Sterling_to_other(double amt) {
+    public static void Dollar_to_other(double amt) { // Création d'une méthode pour convertir le dollar en d'autres monnaie
 
-        System.out.println(Math.round(amt)+" Livre sterling = " + Math.round((amt*1.17*100.0)) / 100.0 + " Euro");
+        System.out.println(Math.round(amt)+" Dollar = " + Math.round(amt*0.88*100.0) / 100.0 + " Euro");
         System.out.println();
 
-        System.out.println(Math.round(amt)+" Livre sterling = " + Math.round((amt*1.33*100.0)) / 100.0 + " Dollar");
+        System.out.println(Math.round(amt)+" Dollar = " + Math.round(amt*0.75*100.0) / 100.0 + " Livre sterling");
+        System.out.println();
+
+        System.out.println(Math.round(amt)+" Dollar = " + Math.round(amt*143.97*100.0) / 100.0 + " Yen");
     }
+
+    public static void Sterling_to_other(double amt) { // Création d'une méthode pour convertir le livre sterling en d'autres monnaie
+
+        System.out.println(Math.round(amt)+" Livre sterling = " + Math.round(amt*1.17*100.0) / 100.0 + " Euro");
+        System.out.println();
+
+        System.out.println(Math.round(amt)+" Livre sterling = " + Math.round(amt*1.33*100.0) / 100.0 + " Dollar");
+        System.out.println();
+
+        System.out.println(Math.round(amt)+" Livre sterling = " + Math.round(amt*191.34*100.0) / 100.0 + " Yen");
+    }
+
+    public static void Yen_to_other(double amt) { // Création d'une méthode pour convertir le yen en d'autres monnaie
+
+        System.out.println(Math.round(amt)+" Yen = " + Math.round(amt*0.0061*100.0) / 100.0 + " Euro");
+        System.out.println();
+
+        System.out.println(Math.round(amt)+" Yen = " + Math.round(amt*0.0069*100.0) / 100.0 + " Dollar");
+        System.out.println();
+
+        System.out.println(Math.round(amt)+" Yen = " + Math.round(amt*0.0052*100.0) / 100.0 + " Livre sterling");
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("1 EUR : Euros");
+        System.out.println("1 EUR : Euros"); // Affiche les choix proposés
         System.out.println("2 USD : Dollars");
         System.out.println("3 GBP : Livres Sterling");
+        System.out.println("4 JPY : Yen");
         
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Choisissez votre devise en tapant un des chiffres ci-dessus :");
-        int choice = sc.nextInt();
+        int choice = sc.nextInt(); // Le choix proposé par l'utilisateur
 
         System.out.println("Entrez votre montant :");
-        double amount = sc.nextDouble();
+        double amount = sc.nextDouble(); // Le montant donné par l'utilisateur
 
-        switch (choice) {
+        switch (choice) { // Choisi la méthode en fonction du choix donné par l'utilisateur
             case 1:
                 Euro_to_other(amount);
                 break;
@@ -82,6 +104,9 @@ public class exo15 {
                 break;
             case 3:
                 Sterling_to_other(amount);
+                break;
+            case 4:
+                Yen_to_other(amount);
                 break;
             default:
                 System.out.println("Ne tapez pas n'importe quoi !");
