b422456618e843d4992c84401217b91347d59e69
Kent-cyber <mrkentfire96@gmail.com>
2025-05-20 15:08:46 +0200

    Code modifier pour épurer le code

--- Jalon/Cas_Pratique_3.java
+++ Jalon/Cas_Pratique_3.java
@@ -21,11 +21,11 @@ public class Cas_Pratique_3 {
         System.out.println("A combien d'années date votre dernier contrôle technique (0 s'il est moins d'un an) :");
         controle = sc.nextInt();
 
-        if (vitesse > 130 || collision == true) {
-            System.out.println("Vous n'êtes pas éligibles pour le remplacement de votre voiture");
+        if (vitesse < 130 && collision == true && (compteur > 150000 && controle <= 2 || compteur < 150000 && controle <= 5)) {
+            System.out.println("Vous êtes bien éligibles pour le remplacement de votre voiture");
 
-        } else if (compteur > 150000 && controle <= 2 || compteur < 150000 && controle <= 5) {
-                System.out.println("Vous êtes bien éligibles pour le remplacement de votre voiture");
+        } else {
+                System.out.println("Vous n'êtes pas éligibles pour le remplacement de votre voiture");
 
         }
 
